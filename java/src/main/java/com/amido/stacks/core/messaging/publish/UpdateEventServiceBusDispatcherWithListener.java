2638106a327f5f26d86c731147762724f73c1624
John Salliaris <john.salliaris@amido.com>
2021-09-16 15:54:39 +0100

    format changes

--- java/src/main/java/com/amido/stacks/core/messaging/publish/UpdateEventServiceBusDispatcherWithListener.java
+++ java/src/main/java/com/amido/stacks/core/messaging/publish/UpdateEventServiceBusDispatcherWithListener.java
@@ -49,7 +49,8 @@ public class UpdateEventServiceBusDispatcherWithListener
               error ->
                   logger.error(
                       "Error when sending message: Id = {}, error: {}",
-                      serviceBusMessage.getMessageId(), error),
+                      serviceBusMessage.getMessageId(),
+                      error),
               () -> logger.info("Message sent: Id = {}", serviceBusMessage.getMessageId()));
 
     } catch (JsonProcessingException e) {
