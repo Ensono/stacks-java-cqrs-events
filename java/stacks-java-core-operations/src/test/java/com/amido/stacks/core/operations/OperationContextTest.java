977532b38db72a0ae9e156e4700a825b0bff42d8
guillermo <guillonrg@gmail.com>
2021-10-21 12:25:52 +0100

    Add Operations module and move classes from core

---
+++ java/stacks-java-core-operations/src/test/java/com/amido/stacks/core/operations/OperationContextTest.java
@@ -0,0 +1,29 @@
+package com.amido.stacks.core.operations;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.UUID;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@Tag("Component")
+public class OperationContextTest {
+
+    private final static int OPERATION_CODE = 100;
+
+    @Test
+    void shouldReturnRightOperationCodeAndCorrelationId() {
+
+        var uuid = UUID.randomUUID();
+
+        OperationContext operationContext = Mockito.mock(OperationContext.class, Mockito.withSettings()
+                .useConstructor(OPERATION_CODE, uuid.toString())
+                .defaultAnswer(Mockito.CALLS_REAL_METHODS)
+        );
+
+        then(operationContext.getOperationCode()).isEqualTo(OPERATION_CODE);
+        then(operationContext.getCorrelationId()).isEqualTo(uuid.toString());
+    }
+
+}
