665f43f8c469ae2966894dbe4a8ab1ef29ed9010
guillermo <guillonrg@gmail.com>
2021-10-21 16:25:16 +0100

    Add messaging module

---
+++ java/stacks-java-core-messaging/src/test/java/com/amido/stacks/core/messaging/publish/TestApplicationEvent.java
@@ -0,0 +1,10 @@
+package com.amido.stacks.core.messaging.publish;
+
+import com.amido.stacks.core.messaging.event.ApplicationEvent;
+
+public class TestApplicationEvent extends ApplicationEvent {
+
+    public TestApplicationEvent(int operationCode, String correlationId, int eventCode) {
+        super(operationCode, correlationId, eventCode);
+    }
+}
--- java/stacks-java-core-messaging/src/test/java/com/amido/stacks/core/messaging/publish/UpdateEventServiceBusDispatcherWithListenerTest.java
+++ java/stacks-java-core-messaging/src/test/java/com/amido/stacks/core/messaging/publish/UpdateEventServiceBusDispatcherWithListenerTest.java
@@ -1,40 +1,47 @@
 package com.amido.stacks.core.messaging.publish;
 
-import static org.assertj.core.api.BDDAssertions.then;
-
 import com.amido.stacks.core.messaging.listen.DefaultEventListener;
-import com.amido.stacks.menu.commands.CreateCategoryCommand;
-import com.amido.stacks.menu.events.MenuCreatedEvent;
 import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusSenderAsyncClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import java.util.UUID;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.UUID;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
 @Tag("Component")
+@ExtendWith(MockitoExtension.class)
 class UpdateEventServiceBusDispatcherWithListenerTest {
+
+  private JsonMapper jsonMapper;
 
   @MockBean ServiceBusSenderAsyncClient topicAsyncSender;
 
+  private final static int OPERATION_CODE = 100;
+
+  private final static int EVENT_CODE = 200;
+
   @Test
   void testCreateMessage() throws JsonProcessingException {
+
+    var uuid = UUID.randomUUID();
 
     // Given
     UpdateEventServiceBusDispatcherWithListener d =
         new UpdateEventServiceBusDispatcherWithListener(
             topicAsyncSender, JsonMapper.builder().build(), new DefaultEventListener());
+    TestApplicationEvent testApplicationEvent = new TestApplicationEvent(OPERATION_CODE, uuid.toString(), EVENT_CODE);
 
     // When
-    CreateCategoryCommand command =
-        new CreateCategoryCommand("CorrelationId", UUID.randomUUID(), "name", "description");
-    MenuCreatedEvent applicationEvent = new MenuCreatedEvent(command, command.getMenuId());
-
-    ServiceBusMessage message = d.createServiceBusMessageFromEvent(applicationEvent);
+    ServiceBusMessage message = d.createServiceBusMessageFromEvent(testApplicationEvent);
 
     // Then
-    then(message.getMessageId()).isEqualTo(applicationEvent.getId().toString());
+    then(message.getMessageId()).isEqualTo(testApplicationEvent.getId().toString());
   }
 }
