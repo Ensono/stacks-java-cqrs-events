87e8aa58d0de5733b759b29ab3e1338fbc41af18
guillermo <guillonrg@gmail.com>
2021-10-21 12:41:51 +0100

    Add bash scripts to run and test the application

---
+++ java/run_tests.sh
@@ -0,0 +1,4 @@
+mvn -f ../api-tests/pom.xml clean verify
+open ../api-tests/target/site/serenity/index.html
+mvn -f ../api-tests-karate/pom.xml clean test
+open ../api-tests-karate/target/surefire-reports/karate-summary.html
