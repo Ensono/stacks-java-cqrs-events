e22b349c7cd4c26e028d818cdb5f6931e7083b47
John Salliaris <john.salliaris@amido.com>
2021-10-27 11:46:31 +0100

    Dockerfile build/run script trial added componentscan in Application.java

--- java/sample-cqrs-events/src/main/java/com/amido/stacks/Application.java
+++ java/sample-cqrs-events/src/main/java/com/amido/stacks/Application.java
@@ -3,10 +3,11 @@ package com.amido.stacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.amido.stacks.core")
 @PropertySource("classpath:auth.properties")
 public class Application {
   public static void main(String[] args) {
