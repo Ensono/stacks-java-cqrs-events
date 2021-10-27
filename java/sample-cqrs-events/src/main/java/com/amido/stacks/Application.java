55800f17058519dcb86a144dc056dd09dad87579
John Salliaris <john.salliaris@amido.com>
2021-10-27 11:55:17 +0100

    removed componentscan from Application.java

--- java/sample-cqrs-events/src/main/java/com/amido/stacks/Application.java
+++ java/sample-cqrs-events/src/main/java/com/amido/stacks/Application.java
@@ -3,11 +3,10 @@ package com.amido.stacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(scanBasePackages = "com.amido.stacks.core")
+@SpringBootApplication
 @PropertySource("classpath:auth.properties")
 public class Application {
   public static void main(String[] args) {
