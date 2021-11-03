2b40df1d95cbaeb5bf1d8157e3b6a0e3e506af8b
guillermo <guillonrg@gmail.com>
2021-11-03 11:20:56 +0000

    Updates to remove amido dependencies in the archetype

--- java/build_archetype.sh
+++ java/build_archetype.sh
@@ -1,2 +1,3 @@
+rm -rf archetype
 mvn clean archetype:create-from-project -DpropertyFile=archetype.properties
-mvn install -f target/generated-sources/archetype/pom.xml
+mvn install -f archetype/pom.xml
