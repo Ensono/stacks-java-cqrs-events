96d666cf32fdc6103ff0a518f6ec00a83105a4c7
guillermo <guillonrg@gmail.com>
2021-11-23 12:43:48 +0000

    Rename packages to com.amido.stacks.workloads and tests

--- api-tests/src/test/java/com/amido/stacks/core/api/status/AppStatus.java
+++
@@ -1,6 +0,0 @@
-package com.amido.stacks.core.api.status;
-
-public enum AppStatus {
-  RUNNING,
-  DOWN
-}
---
+++ java/src/main/java/com/amido/stacks/workloads/menu/handlers/DeleteCategoryHandler.java
@@ -0,0 +1,52 @@
+package com.amido.stacks.workloads.menu.handlers;
+
+import com.amido.stacks.core.messaging.publish.ApplicationEventPublisherWithListener;
+import com.amido.stacks.workloads.menu.commands.DeleteCategoryCommand;
+import com.amido.stacks.workloads.menu.domain.Category;
+import com.amido.stacks.workloads.menu.domain.Menu;
+import com.amido.stacks.workloads.menu.events.CategoryDeletedEvent;
+import com.amido.stacks.workloads.menu.events.MenuEvent;
+import com.amido.stacks.workloads.menu.events.MenuUpdatedEvent;
+import com.amido.stacks.workloads.menu.exception.CategoryDoesNotExistException;
+import com.amido.stacks.workloads.menu.repository.MenuRepository;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+
+/** @author ArathyKrishna */
+@Component
+public class DeleteCategoryHandler extends MenuBaseCommandHandler<DeleteCategoryCommand> {
+
+  public DeleteCategoryHandler(
+      MenuRepository menuRepository,
+      ApplicationEventPublisherWithListener applicationEventPublisher) {
+    super(menuRepository, applicationEventPublisher);
+  }
+
+  Optional<UUID> handleCommand(Menu menu, DeleteCategoryCommand command) {
+    Category category = getCategory(menu, command);
+    List<Category> collect =
+        menu.getCategories().stream()
+            .filter(t -> !Objects.equals(t, category))
+            .collect(Collectors.toList());
+    menu.setCategories(!collect.isEmpty() ? collect : Collections.emptyList());
+    menuRepository.save(menu);
+    publishEvents(raiseApplicationEvents(menu, command));
+    return Optional.empty();
+  }
+
+  List<MenuEvent> raiseApplicationEvents(Menu menu, DeleteCategoryCommand command) {
+    return Arrays.asList(
+        new MenuUpdatedEvent(command), new CategoryDeletedEvent(command, command.getCategoryId()));
+  }
+
+  Category getCategory(Menu menu, DeleteCategoryCommand command) {
+    return findCategory(menu, command.getCategoryId())
+        .orElseThrow(() -> new CategoryDoesNotExistException(command, command.getCategoryId()));
+  }
+}
