ea177360fd93a59195531f22f2d42a4217a580ae
Steve Davis <steve.davis@amido.com>
2022-06-28 13:49:25 +0100

    Updated formatting

--- java/src/test/java/com/amido/stacks/workloads/actuator/ActuatorTest.java
+++ java/src/test/java/com/amido/stacks/workloads/actuator/ActuatorTest.java
@@ -25,19 +25,20 @@ import org.springframework.test.context.TestPropertySource;
     classes = Application.class)
 @TestPropertySource(properties = {"management.port=0"})
 @EnableAutoConfiguration(
-    exclude = {CosmosRepositoriesAutoConfiguration.class, CosmosAutoConfiguration.class,
-        CosmosHealthConfiguration.class})
+    exclude = {
+      CosmosRepositoriesAutoConfiguration.class,
+      CosmosAutoConfiguration.class,
+      CosmosHealthConfiguration.class
+    })
 @Tag("Component")
 class ActuatorTest {
 
   @Value("${local.management.port}")
   private int mgt;
 
-  @Autowired
-  private TestRestTemplate testRestTemplate;
+  @Autowired private TestRestTemplate testRestTemplate;
 
-  @MockBean
-  private MenuRepository menuRepository;
+  @MockBean private MenuRepository menuRepository;
 
   @Test
   void shouldReturn200WhenSendingRequestToHealthEndpoint() {
