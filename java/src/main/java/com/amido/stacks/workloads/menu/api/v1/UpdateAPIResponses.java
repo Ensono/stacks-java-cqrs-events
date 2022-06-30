3c3115a3b895a0c6bbc702489a346c8792ac9cd6
Suresh Krishnan <suresh.krishnan@amido.com>
2022-06-30 12:24:02 +0100

    task-4714: initial commit refactor controller annotations

---
+++ java/src/main/java/com/amido/stacks/workloads/menu/api/v1/UpdateAPIResponses.java
@@ -0,0 +1,53 @@
+package com.amido.stacks.workloads.menu.api.v1;
+
+import com.amido.stacks.core.api.dto.ErrorResponse;
+import com.amido.stacks.workloads.menu.api.v1.dto.response.ResourceUpdatedResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+@ApiResponse(
+    responseCode = "200",
+    description = "Success",
+    content =
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ResourceUpdatedResponse.class)))
+@ApiResponse(
+    responseCode = "204",
+    description = "No Content",
+    content =
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)))
+@ApiResponse(
+    responseCode = "400",
+    description = "Bad Request",
+    content =
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)))
+@ApiResponse(
+    responseCode = "401",
+    description = "Unauthorized, Access token is missing or invalid",
+    content =
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)))
+@ApiResponse(
+    responseCode = "403",
+    description = "Forbidden, the user does not have permission to execute this operation",
+    content =
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)))
+@ApiResponse(
+    responseCode = "404",
+    description = "Resource not found",
+    content =
+        @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)))
+@SecurityRequirement(name = "bearerAuth")
+public @interface UpdateAPIResponses {}
