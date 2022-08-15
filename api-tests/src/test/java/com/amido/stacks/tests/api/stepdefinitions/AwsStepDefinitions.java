7b58e3bfd6dbb10332e431494f35a538830aaa83
YordanAngelov <yordan.angelov@amido.com>
2022-08-15 12:18:20 +0100

    cleaning up and making sure naming is consistent with other repos

--- api-tests/src/test/java/com/amido/stacks/tests/api/stepdefinitions/AwsStepDefinitions.java
+++ api-tests/src/test/java/com/amido/stacks/tests/api/stepdefinitions/AwsStepDefinitions.java
@@ -1,13 +1,11 @@
 package com.amido.stacks.tests.api.stepdefinitions;
 
-import com.amido.stacks.tests.api.status.AwsFeaturesStatus;
+import com.amido.stacks.tests.api.cloud.AwsFeaturesStatus;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
-import org.hamcrest.Matchers;
 
 import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class AwsStepDefinitions {
@@ -16,7 +14,7 @@ public class AwsStepDefinitions {
 
   @When("I check the example secrets")
   public void check_the_example_secrets() {
-    AwsFeaturesStatus.readStatusMessage();
+    AwsFeaturesStatus.readExampleSecrets();
   }
 
   @Then("the API should return the correct examples")
